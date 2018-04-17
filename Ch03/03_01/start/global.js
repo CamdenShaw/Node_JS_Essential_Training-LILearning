7a7ffb2df96a57ab25465cd533f5bb192a02bc29
Camden_Shaw <camden.shaw@gmail.com>
2018-04-17 12:13:48 -0700

    Finished Lesson 1 from chapter 3 - Node.js Essential Training.

---
+++ Ch03/03_01/start/global.js
@@ -0,0 +1,11 @@
+const path = require("path")
+
+// const hello = "Hello World from Node.js"
+
+// let justNode = hello.slice(17)
+
+console.log(`Rock on World from ${path.basename(__filename)}`)
+
+// console.log(__dirname)
+
+// console.log(__filename)
